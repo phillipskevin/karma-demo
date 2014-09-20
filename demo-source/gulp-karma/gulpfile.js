8f6e37c448f1d9015dbd1c2a2af756e80ff32919
Kevin Phillips <kphillips@cars.com>
2014-09-20 17:00:16 -0500

    adding command line options for coverage, watch, and browsers

--- demo-source/gulp-karma/gulpfile.js
+++ demo-source/gulp-karma/gulpfile.js
@@ -8,6 +8,29 @@ gulp.task('test', function () {
         action: 'run'
     };
 
+    // --coverage will create html coverage report
+    // otherwise coverage summary will be shown
+    if (argv.coverage) {
+        options.coverageReporter = {
+            type : 'html',
+            dir : 'karma-coverage/'
+        }
+    }
+
+    // --watch will run karma:watch
+    if (argv.watch) {
+        options.action = 'watch';
+        // turn off coverage to avoid obfuscation
+        options.reporters = ['dots']
+    }
+
+    // --browsers=... will set which browsers to use
+    // --browsers=Chrome,Firefox will run tests in Chrome and Firefox
+    // default is PhantomJS
+    if (argv.browsers) {
+        options.browsers = argv.browsers.split(',');
+    }
+
 	gulp.src(['nope.js'])
 		.pipe(karma(options));
 });
